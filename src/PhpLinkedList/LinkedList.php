3f1d26b4b168b354c5fb3579662e87c571d8aa61
Yuuki Takao <takaaaao3@gmail.com>
2019-11-09 23:53:38 +0900

    Executed from node data and  size acquisition

--- src/PhpLinkedList/LinkedList.php
+++ src/PhpLinkedList/LinkedList.php
@@ -1,41 +1,78 @@
 <?php declare(strict_types=1);
 
 namespace PhpLinkedList;
+
 use PhpLinkedList\Node;
+use Exception;
+
 
 class LinkedList
 {
-    private $first_node = null;
-    private $size = null;
+    private $first_node;
+    private $size;
 
     public function __construct()
     {
         $this->first_node = new Node(null);
+        $this->size = 0;
     }
 
-    public function insert()
+    public function insert($data, int $order_number=1): bool
     {
+        if ($order_number < 1) {
+            throw new Exception('指定できるのは1以上の整数のみです');
+        }
+        $previous_node = $this->getNode($order_number - 1);
+        if (is_null($previous_node)) {
+            return false;
+        }
+        $new_node = new Node($data);
 
+        if (! is_null($previous_node->getNextNode())) {
+            $new_node->setNextNode($previous_node->getNextNode());
+        }
+        $previous_node->setNextNode($new_node);
+
+        return true;
     }
 
-    public function get()
+    public function get(int $position)
     {
-
+        $target_node = $this->getNode($position);
+        if (is_null($target_node)) {
+            // throw new Exception("指定の場所に要素が存在しません");
+            var_dump('指定の要素はありません');
+            return null;
+        }
+        
+        return $target_node->getData();
     }
 
     public function size()
     {
-        if (isset($this->size)) {
-            return $this->size;
-        } else {
-            $current_node = $this->first_node->getNextNode();
-            $length = 0;
-            while($current_node != null) {
-                ++$length;
-                $current_node = $current_node->getNextNode();
-            }
-            return $length;
+        $current_node = $this->first_node->getNextNode();
+        $length = 0;
+        while($current_node != null) {
+            $length++;
+            $current_node = $current_node->getNextNode();
         }
+        return $length;
+    }
+
+    private function getNode(int $position): Node
+    {
+        $current_node = $this->first_node;
+        
+        for ($i = 0; $i < $position; $i++) {
+            if (is_null($current_node)) {
+                return null;
+            }
+            $current_node = $current_node->getNextNode();
+        }
+
+        // var_dump('hogehoge');
+        // var_dump($current_node);
+        return $current_node;
     }
 }
 
--- tests/LinkedListTest.php
+++ tests/LinkedListTest.php
@@ -7,11 +7,21 @@ use PhpLinkedList\LinkedList;
 
 class LinkedListTest extends TestCase
 {
+    public function testGet()
+    {
+        $linked_list = new LinkedList();
+        $linked_list->insert('One');
+        $linked_list->insert('Two');
+        $this->assertEquals($linked_list->get(2), 'One');
+        $this->assertEquals($linked_list->get(1), 'Two');
+    }
+
     public function testSize()
     {
         $linked_list = new LinkedList();
         $linked_list->insert("One");
         $linked_list->insert("Two");
-        $this->assertEquals($linked_list->size(), 2);
+        $linked_list->insert("Three");
+        $this->assertEquals($linked_list->size(), 3);
     }
 }
